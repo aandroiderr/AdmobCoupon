c299f2750c963f0c5e7fb555a2e3108d2590c4d7
Kiet Le <lvankiet@gmail.com>
2018-03-02 21:59:40 +0700

    kl

--- AdmobCocos/Classes/HelloWorldScene.cpp
+++ AdmobCocos/Classes/HelloWorldScene.cpp
@@ -26,38 +26,37 @@ static void problemLoading(const char* filename)
 }
 
 void HelloWorld::callbackFromJS(cocos2d::experimental::ui::WebView* webview, const std::string &answer) {
-	std::string response = answer;
-
+    std::string response = answer;
+    CCLOG("%s",response.c_str());
     if (response.find("backbutton.com") != std::string::npos) {
-
-            this->_webView->removeFromParentAndCleanup(true);
-
-
-    }else
-    if (response.find("redeem.com") != std::string::npos) {
-
-    size_t pos = 0;
-std::vector<std::string> vcListVector;
-std::string delimiter="redeem.com";
-    std::string strTmp = response;
-    size_t cutAt;
-    cutAt = strTmp.find(delimiter);
-
+        this->_webView->removeFromParentAndCleanup(true);
+    }else if (response.find("redeem.com") != std::string::npos) {
+        
+        size_t pos = 0;
+        std::vector<std::string> vcListVector;
+        std::string delimiter="redeem.com";
+        std::string strTmp = response;
+        size_t cutAt;
+        cutAt = strTmp.find(delimiter);
+        
         if(cutAt > 0)
         {
             vcListVector.push_back(strTmp.substr(cutAt+delimiter.length()));
         }
-
-
-if(vcListVector.size()>0) {
-    redeemClick( vcListVector.at(0));
-
-
-}}else{
-    adCouponOnClickToUnlock();
-}}
+        
+        
+        if(vcListVector.size()>0) {
+            CCLOG("%s","DONEDONE");
+            redeemClick( urlDecode(vcListVector.at(0)));
+        }
+        
+    }else{
+        adCouponOnClickToUnlock();
+    }
+}
 void HelloWorld::redeemClick(std::string url){
-Application::getInstance()->openURL(url);
+    CCLOG("%s","DONEDDONE");
+    Application::getInstance()->openURL(url);
 }
 
 void HelloWorld::onKeyReleased(EventKeyboard::KeyCode keyCode, Event* event)
@@ -66,8 +65,10 @@ void HelloWorld::onKeyReleased(EventKeyboard::KeyCode keyCode, Event* event)
     {
         if( this->_webView->getParent()){event->stopPropagation();
             this->_webView->removeFromParentAndCleanup(true);
-
-        }else{  Director::getInstance()->end();}
+            
+        }else{
+            Director::getInstance()->end();
+        }
     }
 }
 // on "init" you need to initialize your instance
@@ -108,10 +109,10 @@ bool HelloWorld::init()
     close->addTouchEventListener([this](Ref *ref, ui::Widget::TouchEventType t){
         if (t == ui::Widget::TouchEventType::ENDED) {
         //    AdCoupon::showCoupons();
-            Vec2 origin = Director::getInstance()->getVisibleOrigin();
-            
+
             _webView = experimental::ui::WebView::create();
             _webView->setContentSize(Size(Director::getInstance()->getVisibleSize().width, Director::getInstance()->getVisibleSize().height));
+            Point origin=Director::getInstance()->getVisibleOrigin();
             _webView->setAnchorPoint(Vec2(0,0));
             _webView->setPosition(origin);
             _webView->setJavascriptInterfaceScheme("cocos2dx");
@@ -119,47 +120,17 @@ bool HelloWorld::init()
             _webView->setOnJSCallback(CC_CALLBACK_2(HelloWorld::callbackFromJS,this));
             _webView->setFocusEnabled(false);
             _webView->setFocused(false);
-            //_webView->setScalesPageToFit(true);
             this->addChild(_webView);
         }
     });
     
     addChild(close);
     
-    //Kietle
-    AdmobManager::getInstance()->initialize(this);
-    
-    auto btnInterstitial = ui::Button::create();
-    btnInterstitial->setTitleText("Show Interstitial");
-    btnInterstitial->setTitleFontSize(30);
-    btnInterstitial->setPosition(Vec2(visibleSize.width/2, visibleSize.height - 100));
-    btnInterstitial->addClickEventListener([](Ref* obj){
-        AdmobManager::getInstance()->showInterstitialAds();
-        //AdmobManager::getInstance()->showVideoAds();
-    });
-    addChild(btnInterstitial);
-    
-    btnInterstitial = ui::Button::create();
-    btnInterstitial->setTitleText("Show Video");
-    btnInterstitial->setTitleFontSize(30);
-    btnInterstitial->setPosition(Vec2(visibleSize.width/2, visibleSize.height - 160));
-    btnInterstitial->addClickEventListener([](Ref* obj){
-        AdmobManager::getInstance()->showVideoAds();
-    });
-    addChild(btnInterstitial);
     
     return true;
 }
 
 #pragma mark -AdCouponDelegate
-void HelloWorld::adCouponHasNoCouponsAvailable() {
-    log("no coupons available!");
-    notificationLabel->setString("no coupons available!");
-}
-void HelloWorld::adCouponOnShow() {
-    log("coupons on show!");
-    notificationLabel->setString("coupons on show!");
-}
 void HelloWorld::adCouponOnClickToUnlock() {
     CCLOG("COUPONS");
     log("coupon on click to unlock!");
@@ -183,21 +154,4 @@ void HelloWorld::adCouponOnClickToUnlock() {
     });
     
     this->addChild(close,5);close->setGlobalZOrder(10);
-}
-void HelloWorld::adCouponOnRedirectToLink() {
-    log("coupon on redirect to link");
-    notificationLabel->setString("coupon on redirect to link");
-}
-void HelloWorld::adCouponOnClose() {
-    log("coupon on close");
-    notificationLabel->setString("coupon on close!");
-    
-    auto node = getChildByName("i_watched_ad_button");
-    if (node) {
-        node->removeFromParentAndCleanup(true);
-    }
-}
-void HelloWorld::adCouponsOnRequest() {
-    log("coupons requested");
-    notificationLabel->setString("coupons on request!");
 }
--- AdmobCocos/Classes/HelloWorldScene.h
+++ AdmobCocos/Classes/HelloWorldScene.h
@@ -3,31 +3,72 @@
 
 #include <ui/UIWebView.h>
 #include "cocos2d.h"
-#include "AdmobManager.h"
 
-class HelloWorld : public cocos2d::Scene, public AdmobManagerDelegate
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <iomanip>
+//#include "AdCoupon.hpp"
+
+class HelloWorld : public cocos2d::Scene
 {
 public:
+    
+    std::string urlDecode(const std::string &toDecode) {
+        std::ostringstream out;
+        
+        for(std::string::size_type i=0; i < toDecode.length(); ++i) {
+            if(toDecode.at(i) == '%') {
+                std::string str(toDecode.substr(i+1, 2));
+                out << hexToChar(str);
+                i += 2;
+            } else {
+                out << toDecode.at(i);
+            }
+        }
+        
+        return out.str();
+    }
+    
+    std::string charToHex(unsigned char c) {
+        short i = c;
+        
+        std::stringstream s;
+        
+        s << "%" << std::setw(2) << std::setfill('0') << std::hex << i;
+        
+        return s.str();
+    }
+    
+    unsigned char hexToChar(const std::string &str) {
+        short c = 0;
+        
+        if(!str.empty()) {
+            std::istringstream in(str);
+            
+            in >> std::hex >> c;
+            
+            if(in.fail()) {
+                throw std::runtime_error("stream decode failure");
+            }
+        }
+        
+        return static_cast<unsigned char>(c);
+    }
+
+    
     static cocos2d::Scene* createScene();
 	void callbackFromJS(cocos2d::experimental::ui::WebView* webview, const std::string &answer);
     virtual bool init();
     void onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
     cocos2d::experimental::ui::WebView *_webView;
+    
     // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
     void redeemClick(std::string url);
-     void adCouponHasNoCouponsAvailable();
-     void adCouponOnShow();
     void adCouponOnClickToUnlock();
-     void adCouponOnRedirectToLink();
-     void adCouponOnClose();
-     void adCouponsOnRequest();
+
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
-    
-    
-    virtual void AdmobManagerOnInterstitialClose(){cocos2d::log("===interstitial close");};
-    virtual void AdmobManagerOnVideoClose(){cocos2d::log("===video close");};
 };
 
 #endif // __HELLOWORLD_SCENE_H__
